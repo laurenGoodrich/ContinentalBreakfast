d4bf1a13de49c990af72bc616e3e942fbc963a0d
mgcarlson <mgcarlson@arizona.edu>
2024-12-06 16:32:31 -0700

    ecology sections all done, formatting needed

---
+++ antarctica/antarcticaQuiz.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.clickable-image').forEach(image => {
+    image.addEventListener('click', () => {
+      const funFact = image.nextElementSibling;
+      funFact.style.display = funFact.style.display === 'none' ? 'block' : 'none';
+    });
+  });
+  
